bb223e63f31497fa8a90d3ee0ebb3de212edff3a
McIvor Steiner <mcivorsteiner@gmail.com>
2014-05-17 12:03:14 -0700

    added initial rails structure 3.2.13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kivorflow::Application.config.secret_token = '110d768cf2616a6a03c2070cdcfbbde0882f7fb1a940491454c08742b9c6b2d337eaf557015cd79caf5dcebee776fbe7bc1a496c99212b19cc2a5eaebed12e52'
