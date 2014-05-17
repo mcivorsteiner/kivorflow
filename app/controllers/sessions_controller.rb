059aaf40f3e6bdede0985444ead156e5e52e2a9a
McIvor Steiner <mcivorsteiner@gmail.com>
2014-05-17 16:31:21 -0700

    added login functionality with session create action and new form, also added user validations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      redirect_to root_path
+    end
   end
 
   def destroy
+    session.clear
+    redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :full_name, :password, :password_confirmation
-  validates_presence_of :email, :full_name, :password_digest
+
+  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+  validates :full_name, :presence => true
+  validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
+  validates_presence_of :password, :on => :create
+  
   has_secure_password
 
 end
