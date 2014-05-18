b7f16381daadf8bbc13ff7efa2b5eb9b0fd63f98
McIvor Steiner <mcivorsteiner@gmail.com>
2014-05-17 20:30:30 -0700

    created display and form for questions, can succesfully create questions from profile

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,12 +3,21 @@ class QuestionsController < ApplicationController
   end
 
   def show
+    @question = Question.find_by_id(params[:id])
   end
 
   def new
   end
 
   def create
+    @user = User.find_by_id(session[:user_id])
+    @question = Question.new(params[:question])
+    if @user && @question.save
+      @user.questions << @question
+      redirect_to @user
+    else
+      redirect_to @user
+    end
   end
 
   def edit
