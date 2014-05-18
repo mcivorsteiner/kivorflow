416c4ed767d4d22e017593663d784cce24fe9bcd
McIvor Steiner <mcivorsteiner@gmail.com>
2014-05-18 01:57:13 -0700

    add javascript so the create comment form is initially hidden, then can be shown when add comment link is clicked

--- app/assets/javascripts/home_controller.js
+++ app/assets/javascripts/home_controller.js
@@ -5,5 +5,6 @@ function HomeController(homeView){
 HomeController.prototype = {
   init: function(){
     $(this.view.signUpLinkSelector).on('click', this.view.showSignUpForm.bind(this.view))
+    $(this.view.commentLinkSelector).on('click', this.view.showCommentForm.bind(this.view))
   }
 }
--- app/assets/javascripts/home_view.js
+++ app/assets/javascripts/home_view.js
@@ -2,11 +2,19 @@ function HomeView(){
   this.loginFormSelector = '#login-form'
   this.signUpLinkSelector = '#sign-up-link'
   this.signUpDivSelector = '#sign-up-div'
+  this.commentLinkSelector = ".comment-form-link"
+  this.commentDivSelector = ".comment-form ~ div"
 }
 
 HomeView.prototype = {
   showSignUpForm: function(){
     event.preventDefault()
     $(this.signUpDivSelector).toggleClass("hidden")
+  },
+
+  showCommentForm: function(){
+    console.log("hello")
+    event.preventDefault()
+    $(event.target).next().toggleClass("hidden")
   }
 }
