2e633bf1feee0e0e6227531f2be740bead8cb09f
McIvor Steiner <mcivorsteiner@gmail.com>
2014-05-17 21:02:32 -0700

    added ability to edit and delete questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Question.all
   end
 
   def show
@@ -21,11 +22,21 @@ class QuestionsController < ApplicationController
   end
 
   def edit
+    @question = Question.find(params[:id])
   end
 
   def update
+    @question = Question.find(params[:id])
+    if @question.update_attributes(params[:question])
+      redirect_to question_path(@question)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to user_path(User.find(session[:user_id]))
   end
 end
