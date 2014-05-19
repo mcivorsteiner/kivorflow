7e0532b69fd740484b392b5672df2a47667b1123
McIvor Steiner <mcivorsteiner@gmail.com>
2014-05-18 19:36:27 -0700

    created migration to change the content fields in the questions, answers and comments tables to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140518041208) do
+ActiveRecord::Schema.define(:version => 20140519022940) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
     t.integer  "user_id"
-    t.string   "content"
+    t.text     "content"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
-    t.string   "content"
+    t.text     "content"
     t.integer  "commentable_id"
     t.string   "commentable_type"
     t.datetime "created_at",       :null => false
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(:version => 20140518041208) do
   create_table "questions", :force => true do |t|
     t.integer  "user_id"
     t.string   "title"
-    t.string   "content"
+    t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
