5995bcb6664b9d26c4916173a0009ec582345941
McIvor Steiner <mcivorsteiner@gmail.com>
2014-06-11 01:20:41 -0700

    added comments_controller_spec, made two tests for create action, modified comments controller action to use current_user helper method

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe CommentsController do
+  let!(:user) { FactoryGirl.create :user }
+  let!(:question) { FactoryGirl.create :question }
+  let!(:answer) { FactoryGirl.create :answer }
+  before(:each) { stub_current_user(user) }
+
+  context "create" do
+    it "should create a comment if attributes valid" do
+      expect {
+        post :create, comment: { commentable_type: "Answer", commentable_id: answer.id, content: "Miss America" }
+      }.to change{Comment.count}.by(1)
+    end
+
+    it "should add association to correct answer" do
+      expect {
+        post :create, comment: { commentable_type: "Answer", commentable_id: answer.id, content: "Miss America" }
+      }.to change{answer.comments.count}.by(1)
+    end
+  end
+end
