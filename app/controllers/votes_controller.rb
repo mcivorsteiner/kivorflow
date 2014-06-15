708d55ed562602ad5feb3b795954aef84741dd0e
McIvor Steiner <mcivorsteiner@gmail.com>
2014-06-15 17:00:27 -0500

    users can only vote on item once, and need to be logged in to vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,11 @@
 class VotesController < ApplicationController
   def create
-    @vote = current_user.votes.create(params[:vote])
-    if @vote.save
+    if logged_in? && current_user.votes.where(votable_type: params["vote"]["votable_type"], votable_id: params["vote"]["votable_id"]).empty?
+      @vote = current_user.votes.create(params[:vote])
       render :json => { vote_total: @vote.votable.vote_total , votable_type: @vote.votable_type.downcase, votable_id: @vote.votable_id }
     else
-      render :json => { errors: @vote.errors.full_messages }
+      error = logged_in? ? "can only vote once" : "log in to vote"
+      render :text => error, :status => :unprocessable_entity
     end
   end
 end
