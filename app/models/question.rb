82d355618ca68883baf440725698bcd701db8de5
McIvor Steiner <mcivorsteiner@gmail.com>
2014-06-25 19:29:29 -0700

    GREEN added dependency to answers in question model to pass test

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
   attr_accessible :title, :content
   belongs_to :user
-  has_many :answers
+  has_many :answers, dependent: :destroy
   has_many :comments, as: :commentable, dependent: :destroy
   has_many :votes, as: :votable
 
