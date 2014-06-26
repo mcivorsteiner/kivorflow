021c0a2a1079f9d01cd14f78de2d6e7ee8e89b6a
McIvor Steiner <mcivorsteiner@gmail.com>
2014-06-25 19:21:41 -0700

    GREEN added dependency destroy to for comments in the question model to pass test

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ActiveRecord::Base
   attr_accessible :title, :content
   belongs_to :user
   has_many :answers
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
   has_many :votes, as: :votable
 
   validates_presence_of :title, :content
